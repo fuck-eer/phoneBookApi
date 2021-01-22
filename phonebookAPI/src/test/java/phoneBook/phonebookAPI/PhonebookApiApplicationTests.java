b845ca731b009a693550f99e06359342674d2e0d
fuck-eer <nishchaychauhan8@gmail.com>
2021-01-22 21:46:37 +0530

    first

---
+++ phonebookAPI/src/test/java/phoneBook/phonebookAPI/PhonebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package phoneBook.phonebookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
